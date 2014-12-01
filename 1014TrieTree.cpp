d619c2753d18cd3397e64b0aaa4ae10101edeb47
Junbo Xin <jimberxin@gmail.com>
2014-12-01 17:06:42 +0800

    fixed the format space

--- 1014TrieTree.cpp
+++ 1014TrieTree.cpp
@@ -5,59 +5,59 @@ using namespace std;
 const int MAX = 26;
 
 struct TrieNode{
-	int counts;
-	bool isWord;
-	TrieNode* next[MAX];
+    int counts;
+    bool isWord;
+    TrieNode* next[MAX];
 	//constructor
-	TrieNode(){
-		counts = 0;
-		isWord = false;
-		for(int i = 0; i < MAX; ++i)
-			next[i] = NULL;
-	}
+    TrieNode(){
+      counts = 0;
+      isWord = false;
+      for(int i = 0; i < MAX; ++i)
+	next[i] = NULL;
+    }
 };
 
 void insertNode(TrieNode* root, string word){
-	 int n = word.length();
-	 if(root == NULL || n == 0) return;
-	 TrieNode* p = root;
-	 for(int i = 0; i < n; ++i){
-		 if(p -> next[ word[i] - 'a'] == NULL){ //insert the node
-			  p -> next[ word[i]-'a' ] = new TrieNode();
-			  p -> next[ word[i] - 'a'] -> counts  = 1; // counts initializes 1
-		 }else{
-			  p -> next[word[i] - 'a' ] -> counts ++;
-		 }
-		 p = p -> next[ word[i] - 'a' ];
-	 }
-	 p -> isWord = true;
+   int n = word.length();
+   if(root == NULL || n == 0) return;
+   TrieNode* p = root;
+   for(int i = 0; i < n; ++i){
+     if(p -> next[ word[i] - 'a'] == NULL){ //insert the node
+       p -> next[ word[i]-'a' ] = new TrieNode();
+       p -> next[ word[i] - 'a'] -> counts  = 1; // counts initializes 1
+     }else{
+       p -> next[word[i] - 'a' ] -> counts ++;
+     }
+     p = p -> next[ word[i] - 'a' ];
+   }
+   p -> isWord = true;
 
 }
 
 int search(TrieNode*root, string word){
-	int n = word.length();
-	if(root == NULL || n == 0) return 0;
-	TrieNode* p = root;
-	for(int i = 0; i < n; ++i){
-		  if(p -> next[ word[i] - 'a'] == NULL)  return 0;
-		  p = p -> next[ word[i] - 'a' ];		 
-	  }
-	return  (p == NULL? 0: p -> counts);
+   int n = word.length();
+   if(root == NULL || n == 0) return 0;
+   TrieNode* p = root;
+   for(int i = 0; i < n; ++i){
+     if(p -> next[ word[i] - 'a'] == NULL)  return 0;
+     p = p -> next[ word[i] - 'a' ];		 
+   }
+   return  (p == NULL? 0: p -> counts);
 }
 
 int main(void){
-	int nodeNum, testNum;
-	TrieNode* root =  new TrieNode();   // a dummy root  node
-	string words;
-	cin >> nodeNum;
-	for(int i =0; i < nodeNum; ++i){
-		cin >> words;
-		insertNode(root, words);
-	}
-	cin >> testNum;
-	for(int i = 0; i < testNum; ++i){
-		 cin >> words;
-		 cout << search(root,words) << endl;
-	}
-	return 0;
+   int nodeNum, testNum;
+   TrieNode* root =  new TrieNode();   // a dummy root  node
+   string words;
+   cin >> nodeNum;
+   for(int i =0; i < nodeNum; ++i){
+     cin >> words;
+     insertNode(root, words);
+   }
+   cin >> testNum;
+   for(int i = 0; i < testNum; ++i){
+     cin >> words;
+     cout << search(root,words) << endl;
+   }
+   return 0;
 }
--- 1032LongestPalindrome.cpp
+++ 1032LongestPalindrome.cpp
@@ -4,49 +4,49 @@
 using namespace std;
 
 string stringPreprocess(string str){
-	int n = str.length();
-	string result;
-	if(n == 0) return result;
-	result = "$";
-	for(int i = 0; i < n; ++i){
-		result += "#";
-		result += str[i];
-	}
-	result += "#^"; //different with the first one;
-	return result;
+   int n = str.length();
+   string result;
+   if(n == 0) return result;
+   result = "$";
+   for(int i = 0; i < n; ++i){
+     result += "#";
+     result += str[i];
+   }
+   result += "#^"; //different with the first one;
+   return result;
 }
 
 int longestPal(string str){
-	if(str.length() == 0 || str.length() == 1) 
-		return str.length();
-	string newStr = stringPreprocess(str);
-	int n = newStr.length();
-	int *P = new int[n];
-	int right = 0, center = 1, i_mirror;
-	int max_len = 0;
-	for(int i = 1; i < n- 1; ++i){ //from 1 to n -1
-		 i_mirror = 2*center - i;
-		 if(right > i)
-			 P[i] = (right - i > P[i_mirror] )?P[i_mirror]: right - i;
-		 else
-			 P[i] = 0;
-		 while(newStr[i + P[i] +1] == newStr[ i - P[i] -1])
-			     ++P[i];
-		    if(i+P[i] > right){
-				 center = i;
-				 right = i + P[i];
-			}
-			max_len = max(max_len, P[i]);
-	}
-	return max_len;
+   if(str.length() == 0 || str.length() == 1) 
+     return str.length();
+   string newStr = stringPreprocess(str);
+   int n = newStr.length();
+   int *P = new int[n];
+   int right = 0, center = 1, i_mirror;
+   int max_len = 0;
+   for(int i = 1; i < n- 1; ++i){ //from 1 to n -1
+     i_mirror = 2*center - i;
+     if(right > i)
+       P[i] = (right - i > P[i_mirror] )?P[i_mirror]: right - i;
+     else
+       P[i] = 0;
+     while(newStr[i + P[i] +1] == newStr[ i - P[i] -1])
+       ++P[i];
+     if(i+P[i] > right){
+       center = i;
+       right = i + P[i];
+     }
+     max_len = max(max_len, P[i]);
+   }
+   return max_len;
 }
 int main(void){
-	 int testNum;
-	 cin >> testNum;
-	 string str;
-	 for(int i =0; i < testNum; ++i){
-		  cin >> str;
-		  cout << longestPal(str) << endl;
-	 }
-      return 0;
+   int testNum;
+   cin >> testNum;
+   string str;
+   for(int i =0; i < testNum; ++i){
+      cin >> str;
+      cout << longestPal(str) << endl;
+   }
+   return 0;
 }
